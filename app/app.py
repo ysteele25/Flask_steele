384be7e342691a7042bfb54836896f41d438dba6
ysteele <yeda@steelestaffing.com>
2022-08-08 10:10:53 -0500

    app.py reverted to original state without create_app

--- app/app.py
+++ app/app.py
@@ -1,33 +1,22 @@
-#kept both files and they have a small difference on create app
-
 from flask import Flask, appcontext_popped, render_template
 from flask_navigation import Navigation
 #from flask_collect import Collect
 from flask_bootstrap import Bootstrap
 
-def create_app(config):
-  app = Flask(__name__)
-  #app = Flask('app')
-  app.config.from_object(config)
-  
-  Bootstrap(app)
-  Collect(app)
 
-  @app.route('/')
-  def home():
-    return render_template('index.html')
+app = Flask(__name__)
 
-  @app.route("/privacypolicy.html")
-  def privacypolicy():
-        return render_template('privacypolicy.html')
+@app.route('/')
+def home():
+ return render_template('index.html')
 
-  @app.route("/jobs.html")
-  def jobs():
-        return render_template('jobs.html')    
+@app.route("/privacypolicy.html")
+def privacypolicy():
+  return render_template('privacypolicy.html')
 
-  return app
+@app.route("/jobs.html")
+def jobs():
+  return render_template('jobs.html')    
 
 if __name__ == "__main__":
-   app.run(debug=True) 
-
- 
+  app.run(debug=True) 
